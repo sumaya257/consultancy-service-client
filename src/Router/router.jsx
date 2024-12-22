29d04ecfdb9a1a1b2509affef98eff18b878e31d
sumaya257 <sumayaece19@gmai.com>
2024-12-22 13:51:44 +0600

    navbar-design-done

---
+++ src/Router/router.jsx
@@ -0,0 +1,30 @@
+import {createBrowserRouter} from "react-router-dom";
+import Home from "../pages/Home";
+import MainLayout from "../layout/MainLayout";
+import ErrorPage from "../pages/ErrorPage";
+import Register from "../pages/Register";
+import Login from "../pages/Login";
+
+const router = createBrowserRouter([
+    {
+      path: "/",
+      element:<MainLayout></MainLayout>,
+      errorElement:<ErrorPage></ErrorPage>,
+      children:[
+        {
+            path:'/',
+            element:<Home></Home>
+        },
+        {
+            path:'/register',
+            element:<Register></Register>
+        },
+        {
+            path:'/login',
+            element:<Login></Login>
+        }
+      ]
+    },
+  ]);
+
+  export default router
