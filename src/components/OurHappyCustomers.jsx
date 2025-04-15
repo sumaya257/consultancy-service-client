76d4777d0e117cd74fb54f6858889bf92a9061a5
sumaya257 <sumayaece19@gmai.com>
2025-04-15 13:46:17 +0600

    dark-mode-color-upgrade

--- src/components/OurHappyCustomers.jsx
+++ src/components/OurHappyCustomers.jsx
@@ -50,7 +50,7 @@ const OurHappyCustomers = () => {
           {testimonials.map((testimonial, index) => (
             <motion.div
               key={index}
-              className=" p-6 rounded-lg shadow-lg hover:scale-105 transition-transform duration-300"
+              className=" p-6 rounded-lg shadow-lg hover:scale-105 transition-transform duration-300  dark:bg-gray-100  dark:text-black"
               initial={{ opacity: 0, scale: 0.8 }}
               animate={{ opacity: 1, scale: 1 }}
               whileHover={{ scale: 1.05 }}
@@ -63,10 +63,10 @@ const OurHappyCustomers = () => {
                   className="w-16 h-16 rounded-full mr-4"
                 />
                 <div>
-                  <h3 className="font-semibold text-lg text-gray-800 dark:text-white">{testimonial.name}</h3>
+                  <h3 className="font-semibold text-lg text-gray-800 dark:text-black">{testimonial.name}</h3>
                 </div>
               </div>
-              <p className="text-gray-600">{testimonial.feedback}</p>
+              <p className="text-gray-600 dark:text-black">{testimonial.feedback}</p>
             </motion.div>
           ))}
         </motion.div>
